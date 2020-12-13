8909bf866d92cb124fa200b48a0f7beac49d73d9
askripnik <askripnik@carltonone.com>
2020-12-13 13:45:05 -0500

    #112

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>d
-git push -f git@github.com:artemskr/demo.git master:gh-pages
+git push -f git@github.com:artemskr/demo.git master
 
 cd -

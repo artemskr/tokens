02dcc24d54820d1decc300807934cbf44f972e46
Artem Skripnik <askripnik@carlton.ca>
2021-03-16 21:27:26 -0400

    m16

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import TokensList from '@/components/tokens/TokensList.vue'
 
 export default {
   data: [
-    { path: '/', component: TokensList, name: '@tokenslist' }
+    { path: '/', component: TokensList, name: '@tokenslist' },
+    { path: '/demo', component: TokensList, name: '@tokenslist' }
   ]
 }
